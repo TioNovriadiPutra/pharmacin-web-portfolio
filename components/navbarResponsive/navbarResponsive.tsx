aac19571c25ec33ec2206081d54ba998b16e00f2
MikhaelKirenius <92681258+MikhaelKirenius@users.noreply.github.com>
2024-03-29 00:26:29 +0700

    navbar Responsive Progress

--- components/navbarResponsive/navbarResponsive.tsx
+++ components/navbarResponsive/navbarResponsive.tsx
@@ -1,29 +1,43 @@
-import React, { useState } from 'react';
-import style from './navbar.module.css';
+import React,{ useState } from 'react'
+import style from './navbar.module.css'
 
 const NavbarResponsive = () => {
-  const [showMenu, setShowMenu] = useState(false);
+  const [showNavbar, setShowNavbar] = useState(false)
+
+  const handleShowNavbar = () => {
+    setShowNavbar(!showNavbar)
+  }
 
   return (
-    <div className={style.navbar}>
-      <div className={style.logo}>LOGO</div>
-      <div className={style.menu}>
-        <a href="#about">About us</a>
-        <a href="#services">Services</a>
-        <a href="#contact">Contact us</a>
-      </div>
-      <div className={style.menuMobile}>
-        <button onClick={() => setShowMenu(!showMenu)}>☰</button>
-        {showMenu && (
-          <div>
-            <a href="#about">About us</a>
-            <a href="#services">Services</a>
-            <a href="#contact">Contact us</a>
-          </div>
-        )}
+    <div className= {style.navbar}>
+      <div className="container">
+        <div className="logo">
+        </div>
+        <div className="menu-icon" onClick={handleShowNavbar}>
+        ☰
+        </div>
+        <div className={`div-elements  ${showNavbar && 'active'}`}>
+          <ul>
+            <li>
+              <a >Home</a>
+            </li>
+            <li>
+              <a>Blog</a>
+            </li>
+            <li>
+              <a>Projects</a>
+            </li>
+            <li>
+              <a>About</a>
+            </li>
+            <li>
+              <a>Contact</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
-  );
-};
+  )
+}
 
-export default NavbarResponsive;
+export default NavbarResponsive
